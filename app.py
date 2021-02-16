95dd78a80eb3de2eac2ae40849b7ec40a8f90c21
Felix Wortmann <felixwortmann@protonmail.com>
2021-02-16 19:12:53 +0100

    changed default port

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def index():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=443)
